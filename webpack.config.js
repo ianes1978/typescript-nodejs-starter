995993f1874cc6ef95e8def222f993bf848f44e2
unknown <roberto.ianes.78@gmail.com>
2020-05-25 17:53:26 +0200

    add webpack reorganize folders

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const { NODE_ENV = 'production' } = process.env;
+const nodeExternals = require('webpack-node-externals');
+const CopyPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  watch: NODE_ENV === 'development',
+  entry: './src/server.js',
+  mode: NODE_ENV,
+  target: 'node',
+  externals: [nodeExternals()],
+  devtool: 'inline-source-map',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'index.js',
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: ['ts-loader'],
+      },
+    ],
+  },
+  plugins: [new CopyPlugin({ patterns: [{ from: './src/public', to: 'public/' }] })],
+};
