7f42c6ca1552b7bb45b0f08dc6ba9252da44c234
ianes1978 <roberto.ianes.78@gmail.com>
2021-02-28 16:12:05 +0100

    Joi fix update

--- src/store/validators.ts
+++ src/store/validators.ts
@@ -1,22 +1,22 @@
 // const Joi = require("joi");
-import { Joi } from 'joi'
+import * as Joi  from 'joi'
 
 export const validateCreateUser =  (user)=> {
-    const userValidator = {
+    const userValidator = Joi.object({
         firstName: Joi.string().min(3).required(),
         lastName: Joi.string().min(3).required(),
-        email: Joi.string().email({ minDomainAtoms: 2 }).required(),
+        email: Joi.string().email({ minDomainSegments : 2 }).required(),
         isActive: Joi.bool()
-    };
-    return Joi.validate(user, userValidator);
+    });
+    return userValidator.validate(user);
 }
 export const validateEditUser = function (user) {
-    const userValidator = {
+    const userValidator = Joi.object({
         _id: Joi.string().length(24).required(),
         firstName: Joi.string().min(3).required(),
         lastName: Joi.string().min(3).required(),
-        email: Joi.string().email({ minDomainAtoms: 2 }).required(),
+        email: Joi.string().email({ minDomainSegments : 2 }).required(),
         isActive: Joi.bool()
-    };
-    return Joi.validate(user, userValidator);
+    });
+    return userValidator.validate(user);
 }
--- src/tests/validators.spec.js
+++ src/tests/validators.spec.js
@@ -3,22 +3,21 @@ const { validateUser } = require("../store/validators");
 describe("Validators Joy", () => {
   it("Validate User", () => {
     expect(!!validateUser({
-        firstName: '',
-        lastName: 'test',
-        email: 'test@test.it',
-        isActive: true
+      firstName: '',
+      lastName: 'test',
+      email: 'test@test.it',
+      isActive: true
     }).error).toBe(true);
-    
+
     expect(!!validateUser({
-        firstName: 'test',
-        lastName: 'test',
-        email: 'test@test.it',
-        isActive: true
+      firstName: 'test',
+      lastName: 'test',
+      email: 'test@test.it',
+      isActive: true
     }).error).toBe(false);
   });
 });
 
-    
-  
-   
-  
+
+
+
