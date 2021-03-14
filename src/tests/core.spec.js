63cf115e31647c8fba90fa01340de98805e463fd
ianes1978 <roberto.ianes.78@gmail.com>
2021-03-14 13:22:55 +0100

    .

--- src/tests/core.spec.js
+++ src/tests/core.spec.js
@@ -1,10 +1,9 @@
 const coreUsers = require("../core/users");
 
 describe("Core users functions validators", () => {
-    it("Check email function", () => {
-      expect(coreUsers.checkEmail("leonardo.mattevi@email.it")).toBe(true);
-      expect(coreUsers.checkEmail("leonardo.mattevi@email")).toBe(false);
-      expect(coreUsers.checkEmail("leonardo.matteviemail.it")).toBe(false);
-    });
+  it("Check email function", () => {
+    expect(coreUsers.checkEmail("name.surname@email.it")).toBe(true);
+    expect(coreUsers.checkEmail("name.surname@email")).toBe(false);
+    expect(coreUsers.checkEmail("name.surnameemail.it")).toBe(false);
   });
-  
+});
